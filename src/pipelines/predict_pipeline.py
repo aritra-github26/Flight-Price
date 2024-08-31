69fe72a7a80a8ddb3997b103aac1ce734930a087
aritra-github26 <aritra323404@gmail.com>
2024-08-31 21:23:10 +0530

    Prediction Piepline set

--- src/pipelines/predict_pipeline.py
+++ src/pipelines/predict_pipeline.py
@@ -24,13 +24,38 @@ class PredictPipeline:
             raise CustomException(e, sys)
         
 class CustomData:
-    def __init__():
-        pass
+    def __init__(
+        self,
+        source: str,
+        destination: str,
+        airline: str,
+        additional_info: str,
+        total_stops: int,
+        day: int,
+        month: int,
+        duration_min: int
+    ):
+        self.source = source
+        self.destination = destination
+        self.airline = airline
+        self.additional_info = additional_info
+        self.total_stops = total_stops
+        self.day = day
+        self.month = month
+        self.duration_min = duration_min
+
 
     def get_data_as_df(self):
         try:
             input_dict = {
-                
+                'Source': [self.source],
+                'Destination': [self.destination],
+                'Airline': [self.airline],
+                'Additional_Info': [self.additional_info],
+                'Total_Stops': [self.total_stops],
+                'day': [self.day],
+                'month': [self.month],
+                'Duration_min': [self.duration_min]
             }
             return pd.DataFrame(input_dict)
         except Exception as e:
